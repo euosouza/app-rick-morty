ce2e9e7f3d8508dc5a2edc877541182a9f1ab097
victor-souza <euosouza@gmail.com>
2023-08-11 13:47:34 -0300

    corrigindo routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: "/",
+    //   name: "home",
+    //   component: () => import("../views/HomeView.vue"),
+    // },
     {
       path: "/",
-      name: "home",
-      component: () => import("../views/HomeView.vue"),
-    },
-    {
-      path: "/personagens",
       name: "personagens",
       component: () => import("../views/FiltersView.vue"),
     },
