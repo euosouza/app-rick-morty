15b8b24cc094764cd609e2c8108ff35502a1b7e7
victor-souza <euosouza@gmail.com>
2023-08-11 13:48:59 -0300

    criando page home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,53 @@
 <template>
-  <h1>Home</h1>
+  <section id="banner-hero">
+    <Container class="wrapper-banner-hero">
+      <div class="wrapper-text">
+        <h1>
+          RM <span>[App]</span> o melhor site pra saber tudo sobre o multiverso
+          Rick & Morty.
+        </h1>
+        <p>
+          Acompanhe o cientista alcoólatra Rick e seu neto Morty em aventuras
+          interdimensionais cheias de humor, sci-fi e reflexões familiares.
+        </p>
+        <a class="btn" href="#sobre">Saiba mais</a>
+      </div>
+      <img src="../assets/images/banner-hero.png" alt="" />
+    </Container>
+  </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Container from "@/components/ContainerItem.vue";
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/utils.scss";
+
+#banner-hero {
+  .wrapper-banner-hero {
+    @include section-container;
+    .wrapper-text {
+      display: flex;
+      align-items: flex-start;
+      flex-direction: column;
+      gap: 1rem;
+      max-width: 500px;
+
+      a {
+        margin-top: 1rem;
+        text-decoration: none;
+
+        @include responsiveMin($tablet) {
+          width: 240px;
+        }
+      }
+    }
+
+    img {
+      width: 100%;
+      max-width: 360px;
+    }
+  }
+}
+</style>
